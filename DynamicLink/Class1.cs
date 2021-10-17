924effabed5058d44ea90b6cf863a1020b1a6664
Darmin Poturovic <d.poturovic@gmail.com>
2021-10-17 21:27:56 +0200

    Add project files.

---
+++ DynamicLink/Class1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace DynamicLink
+{
+    public class Class1
+    {
+       
+        //Randomize method
+
+        public static int[] Randomize(int[] myRandArray)
+        {
+            //creates a new random object
+            Random rnd = new Random();
+            //condition for loop
+            for(int j = 0; j < myRandArray.Length; j++)
+            {
+                //this randomize array
+                myRandArray[j] = rnd.Next(0, myRandArray.Length);
+            }
+            return myRandArray;
+        }
+
+
+
+        //Swap method  
+        public static void SwapMethod(int[] swapArray, int index1, int index2)
+        {
+            // First step to put index1 in temperary location
+            int tmp = swapArray[index1];
+
+            //Step two to switch places between index1 an index2
+            swapArray[index1] = swapArray[index2];
+
+            //Third step to put index2 in temperary location
+            swapArray[index2] = tmp;
+
+        }
+
+        //Prepare Mothod
+        public static int[] Prepare(int[] data, int SizeOfArray)
+        {
+            data = new int[SizeOfArray];
+            int[] MyArray = Randomize(data);
+            return MyArray;
+        }
+
+        //BUBBLE SORT
+        public static void SortBubble(int[] sortBubleArray)
+        {
+            //Make a loop for each number to be sorted, cancel if the numbers are sorte
+            for (int i = 0; i < sortBubleArray.Length; i++)
+            {
+                //Signal that we are starting a new loop with sorting
+                for (int j = 0; j < sortBubleArray.Length - 1; j++)
+                {
+                    //Move larger numbers in front in the array
+                    if (sortBubleArray[j] > sortBubleArray[j + 1])
+                    {
+                        //using a SwapMethod to swap two digits
+                        SwapMethod(sortBubleArray, j + 1, j);
+                    }
+                }
+            }
+        }
+
+
+        //SELECTION SORT
+        public static void SelectionSort(int[] selectionArray)
+        {
+            //int Min = 0;
+            for( int i = 0; i < selectionArray.Length -1; i++)
+            {
+                int Min = i;
+                for(int j = i + 1; j < selectionArray.Length; j++)
+                {
+                    if(selectionArray[j] < selectionArray[Min])
+                    {
+                        Min = j;
+                    }
+                if(Min != i)
+                    {
+                        SwapMethod(selectionArray, Min, i);
+                    }
+                   
+                }
+            }
+        }
+        //INSERTION SORT
+        public static void InsertionSort(int[] insertionsArray)
+        {
+            int j, temp;
+            //Make a loop for each number to be sorted
+            //start at index 1 becouse we will look "backwards" in the vector
+            for (int i = 1; i <= insertionsArray.Length - 1; i++)
+            {
+                temp = insertionsArray[i];
+                j = i - 1;
+                while(j >= 0 && insertionsArray[j] > temp)
+                {
+                    insertionsArray[j + 1] = insertionsArray[j];
+                    j--;
+                }
+                insertionsArray[j + 1] = temp;
+            }
+        }
+
+        //MERGE SORT: containes two methods: sortmethod and mergemethod
+        
+        //SORT METHOD
+        public static void SortMethod(int[] numbers, int left, int right)
+        {
+            //creates an object
+            int mid;
+            //codition
+            if(right > left)
+            {
+                // passing in the value in object that is middle of an array
+                mid = (right + left) / 2;
+                //Using a sortMethod to sort left side of array
+                SortMethod(numbers, left, mid);
+                //Using a sortMethod to sort right side of array
+                SortMethod(numbers, (mid + 1), right);
+                //using mergeMethod from bellow
+                MergeMethod(numbers, left, (mid + 1), right);
+            }
+        }
+        //MERGE METHOD
+        static public void MergeMethod(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[100];
+            int i, left_end, num_elements, tmp_pos;
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+            while (left <= left_end) temp[tmp_pos++] = numbers[left++];
+            while (mid <= right) temp[tmp_pos++] = numbers[mid++];
+            for(i = 0; i < num_elements; i++)
+            {
+                    numbers[right] = temp[right];
+                    right--;
+            }
+            
+        }
+
+        //QUICK SORT
+        static public void  QuickSort(int[] quickSortArray, int left, int right)
+        {
+            //choose a nuber that divides in larger and smaller numbers
+            int middlepoint = quickSortArray[(left + right) / 2];
+
+            // define variable for each parameter
+            int i = left;
+            int j = right;
+
+            //condition for while loop 
+            while (i <= j)
+            {
+                //Find a number in left part that should be in larger part
+                while (quickSortArray[i] < middlepoint)
+                    i++;
+
+                //Find a number in right side that should be in smaller part
+                while (quickSortArray[j] > middlepoint)  
+                    j--;
+
+
+                //if the condition is still the same
+                if(i <= j)
+                {
+                    //change positions
+                    SwapMethod(quickSortArray, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+                QuickSort(quickSortArray, left, j);
+            if (i < right)
+                QuickSort(quickSortArray, i, right);
+        }
+    }
+}
+                    
+
+                    
+               
+        
+               
+              
+                        
+                
+                   
---
+++ TrainingApp/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using DynamicLink;
+
+namespace TrainingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // A text is printed to consol demanding a number
+            Console.WriteLine("Write the number of Inexes you wish to test");
+            //the input provided is passed into an array
+            int indexes = Convert.ToInt32(Console.ReadLine());
+            //instantiate an object of array
+            int[] Array = new int[indexes];
+            //using a method prepare to randomize array
+            Array = Class1.Prepare(Array, indexes);
+            
+            //prints out and presents alernitives
+            Console.WriteLine("Choose your algorihm!\n\n\n" +
+                "For Bubble Sort, type: 1\n\n" +
+                "For Selection Sort, type: 2\n\n" +
+                "For Quick Selection Sort, type: 3\n\n" +
+                "For Merge Sort, type: 4\n\n" +
+                "For insertion Sort, type: 5\n\n" +
+                "For Lambda sort, type 6 ");
+
+
+
+            //the ipnut provided is passed into an object inpudData
+            int inputData = Convert.ToInt32(Console.ReadLine());
+            //new objact has been created and passed in value
+            int selected = inputData;
+            
+            //Using Switched Statements
+            switch (selected)
+            {
+                //if 1 has been chosen
+                case 1:
+                    //prints out to the consol
+                    Console.WriteLine(" You choosed Buuble Sort!");
+                    //starts measuring time while calling the method SortBubble and using "Array" 
+                    DateTime startTime = DateTime.Now;
+                    Class1.SortBubble(Array);
+                    //stops measuring time
+                    TimeSpan finishTime = DateTime.Now - startTime;
+                    //prints out sorted array and measured time  
+                    Console.WriteLine("[{0}]", string.Join(", ", Array));
+                    Console.WriteLine("It took {0:0.00} ms too sort.\n", finishTime.TotalMilliseconds);
+                    break;
+                // same as in case 1 !!!
+                case 2:
+                    Console.WriteLine(" You choosed Selection Sort!");
+                    DateTime startTimeSelection = DateTime.Now;
+                    Class1.SelectionSort(Array);
+                    TimeSpan finishTimeSelection = DateTime.Now - startTimeSelection;
+                    Console.WriteLine("[{0}]", string.Join(", ", Array));
+                    Console.WriteLine("It took {0:0.00} ms too sort.\n", finishTimeSelection.TotalMilliseconds);
+                    break;
+                // same as in case 1 !!!
+                case 3:
+                    Console.WriteLine(" You choosed Quick Selection Sort!");
+                    DateTime startTimeQuickSelection = DateTime.Now;
+                    Class1.QuickSort(Array, 0, Array.Length - 1);
+                    TimeSpan finishQuickTimeSelection = DateTime.Now - startTimeQuickSelection;
+                    Console.WriteLine("[{0}]", string.Join(", ", Array));
+                    Console.WriteLine("It took {0:0.00} ms too sort.\n", finishQuickTimeSelection.TotalMilliseconds);
+                    break;
+                // same as in case 1 !!!
+                case 4:
+                    Console.WriteLine(" You choosed Merge Sort!");
+                    DateTime startTimeMerge = DateTime.Now;
+                    int len = Array.Length;
+                    Console.WriteLine("MergeSort:");
+
+                    Class1.SortMethod(Array, 0, len - 1);
+
+                    for (int i = 0; i < Array.Length; i++)
+                        Console.WriteLine(Array[i]);
+                    TimeSpan finishMerge = DateTime.Now - startTimeMerge;
+                    Console.WriteLine("It took {0:0.00} ms too sort.\n", finishMerge.TotalMilliseconds);
+                    break;
+                // same as in case 1 !!!
+                case 5:
+                    Console.WriteLine(" You choosed Insertion Sort!");
+                    DateTime startTimeInsertion = DateTime.Now;
+                    Class1.InsertionSort(Array);
+                    Console.WriteLine("[{0}]", string.Join(", ", Array));
+                    TimeSpan finishInsertion = DateTime.Now - startTimeInsertion;
+                    Console.WriteLine("It took {0:0.00} ms too sort.\n", finishInsertion.TotalMilliseconds);
+                    break;
+                case 6:
+                    Console.WriteLine(" You choosed lambda Sort!");
+                   
+                    break;
+
+            }
+        }
+    }
+}
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
